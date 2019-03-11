8e64a80245dd0ee048fa573665f499d360ab9c4f
mukesh_niftywindow <mukesh.patil@niftywindow.com>
2019-03-11 15:15:43 +0530

    adding fa

--- index.php
+++ index.php
@@ -201,6 +201,7 @@
           </style>
           <link rel="stylesheet" type="text/css"  href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,400italic,700,800">
           <link rel="stylesheet" type="text/css"  href="https://fonts.googleapis.com/css?family=Nunito:400,700">
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
           <link rel="stylesheet" type="text/css"  href="https://www.niftywindow.com/clientWeb/css/compiled/window.css">
         </head>
         <body>
