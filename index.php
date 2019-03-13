2459c7241cbcd034c67979e2c6588d8a8fb4a2b0
mukesh_niftywindow <mukesh.patil@niftywindow.com>
2019-03-13 16:31:43 +0530

    passsing params through iframe url

--- index.php
+++ index.php
@@ -75,6 +75,7 @@
         padding: 0;
         font-size: 16px;
         background: #fff;
+        font-family: "Open Sans", Helvetica, Arial, sans-serif, sans;
       }
       .poweredByGoogle {
           position: relative;
@@ -239,7 +240,7 @@
               <p class="alert bg-warning" style="display:none"></p>
             </div>
           </div>
-          <script>
+          <!-- <script>
             var NWAppData = {
               listingId : "5892832569065472",
               brandId : "5282118718455808",
@@ -253,7 +254,7 @@
               hoop : "[{&quot;day&quot;=&quot;Monday&quot;, &quot;time&quot;=&quot;[8:00am-8:15pm]&quot;}, {&quot;day&quot;=&quot;Tuesday&quot;, &quot;time&quot;=&quot;[8:00am-8:15pm]&quot;}, {&quot;day&quot;=&quot;Wednesday&quot;, &quot;time&quot;=&quot;[8:00am-8:15pm]&quot;}, {&quot;day&quot;=&quot;Thursday&quot;, &quot;time&quot;=&quot;[8:00am-8:15pm]&quot;}, {&quot;day&quot;=&quot;Friday&quot;, &quot;time&quot;=&quot;[8:00am-8:15pm]&quot;}, {&quot;day&quot;=&quot;Saturday&quot;, &quot;time&quot;=&quot;[8:00am-8:15pm]&quot;}, {&quot;day&quot;=&quot;Sunday&quot;, &quot;time&quot;=&quot;[]&quot;}]",
               baseUrl : "http://localhost:8888"
           }
-    </script>
+        </script> -->
     <script src="js/require.js"></script>
     <script src="js/require-config.js"></script>
     <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyAmZl1v0HeChOciXnl3Il7RTf-lYjgTtSs&sensor=false&libraries=places"></script>
--- js/script.js
+++ js/script.js
@@ -9,6 +9,20 @@ require([
 	var $address;
 	var mapWidget = new MapWidget();
 	var startingPoint;
+	var NWAppData = {
+		listingId : paramsExtract(listingId),
+		brandId : paramsExtract(brandId),
+		latitude : paramsExtract(latitude),
+		longitude : paramsExtract(longitude)
+	}
+	function paramsExtract(name) {
+		var url = location.href;
+		name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+		var regexS = "[\\?&]"+name+"=([^&#]*)";
+		var regex = new RegExp( regexS );
+		var results = regex.exec( url );
+		return results == null ? null : results[1];
+	}
 
 	function onGeoLocateSuccess(params) {
 		var $btnGMaps = $("#btnGMaps");
